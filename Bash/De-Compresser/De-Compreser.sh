6eb59dd30c45d60d429f6c50a018a140889d81ce
Juan Jose Cunyat Ferrando <joancunyatferrando@gmail.com>
2026-09-16 09:39:48 +0200

    Add functionability to compress with extensions

--- Bash/De-Compresser/De-Compreser.sh
+++ Bash/De-Compresser/De-Compreser.sh
@@ -2,14 +2,14 @@
 
 function ctrl_c() {
     echo -e "\n\n[+] Ending script ... \n"
-    exit 1
+    exit 2
 }
 
 trap ctrl_c INT
 
 function decompress() {
     echo -e "[*]Decompressing file... "
-    sleep 2
+    sleep 3
     filename=$1
     toDecompress=$(7z l $filename | tail -n 3 | head -n 1 | awk 'NF{print $NF}')
     7z x $filename &>/dev/null
@@ -26,23 +26,18 @@ function decompress() {
 function compress() {
     read -p "[?] How may times do you want to compress the file? " n_comp
     
-    if [[ $var =~ ^[0-9]+$ ]]; then
-        cat << 'EOF'
-        7z a file.7z carpeta/         # 7z    -> supports folders
-        7z a -tzip file.zip carpeta/  # zip   -> supports folders
-        7z a -tgzip file.gz archivo   # gzip  -> single file only
-        7z a -tbzip2 file.bz2 archivo # bzip2 -> single file only
-        7z a -ttar file.tar carpeta/  # tar   -> supports folders
-        7z a -twim file.wim carpeta/  # wim   -> supports folders
-EOF
-        while [[ $n_comp < 0 ]]; do
+    if [[ $n_comp =~ ^[0-9]+$ ]]; then
+        number=$n_comp
+        echo -e "7z a file.7z carpeta/         # 7z    -> supports folders\n7z a -tzip file.zip carpeta/  # zip   -> supports folders\n7z a -tgzip file.gz archivo   # gzip  -> single file only\n7z a -tbzip2 file.bz2 archivo # bzip2 -> single file only\n7z a -ttar file.tar carpeta/  # tar   -> supports folders\n7z a -twim file.wim carpeta/  # wim   -> supports folders"
+        while (( number > 0 )); do
             read -p "[?] Extension to compress the file: " exten
             read -p "[?] Name to compress the file: " filecomp
-            if [[ "$exten" == 7z]]; then
+            if [[ "$exten" == "7z" ]]; then
                 7z a $filecomp.7z $filename
             else
                 7z a -t$exten $filecomp.$exten $filename
             fi
+            ((number--))
         done
 
         exit 0
--- Bash/De-Compresser/README.md
+++ Bash/De-Compresser/README.md
@@ -4,4 +4,6 @@ This Script is on making...
 
 **To Do**:
 - Finish Compressed function
+    Change unsuported
+    Manage output  
 - Add error managing on each function
